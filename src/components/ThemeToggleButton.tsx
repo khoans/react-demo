49ed5465e75997bd81a73e7ecc1fbe05464201b0
Khoa Nguyen <khoans1502@gmail.com>
2025-12-02 13:29:24 +0700

    chore

--- src/components/ThemeToggleButton.tsx
+++ src/components/ThemeToggleButton.tsx
@@ -3,9 +3,11 @@ import {useTheme} from "../contexts/ThemeContext";
 function ThemeToggleButton() {
     const {theme, toggleTheme} = useTheme();
 
+    const displayTheme = theme === 'light' ? 'Light' : 'Dark';
+
     return (
         <button onClick={toggleTheme}>
-            Switch to {theme === 'light' ? 'Dark' : 'Light'} Theme
+            Switch to {displayTheme} Theme
         </button>
     );
 }
